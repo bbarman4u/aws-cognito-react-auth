4a58de13ec700b649db9a8effe73c7cb95eb8c82
bbarman4u <iambbarman@gmail.com>
2023-04-17 21:29:07 -0500

    feat: change to use environment entries
    use .env to inject the values

--- src/configs/aws.ts
+++ src/configs/aws.ts
@@ -1,5 +1,5 @@
 export const COGNITO = {
-  REGION: "",
-  USER_POOL_ID: "",
-  APP_CLIENT_ID: "",
+  REGION: process.env.REACT_APP_REGION,
+  USER_POOL_ID: process.env.REACT_APP_USER_POOL_ID,
+  APP_CLIENT_ID: process.env.REACT_APP_USER_CLIENT_ID,
 };
